06f46c2c1c8ad042a56d231168ddf90a072a2bf8
Jia Xin Tan <jiaxintan@Jias-MacBook-Air.local>
2022-12-20 16:34:52 +0800

    Update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards-items-container">
           <CardItem
             path="/services"
-            img="/images/img-9.jpg"
+            img="images/img-9.jpg"
             label="Adventure"
             text="Explore the hidden waterfall deep inside the Amazon Jungle"
           />
           <CardItem
             path="/services"
-            img="/images/img-2.jpg"
+            img="images/img-2.jpg"
             label="Luxury"
             text="Travel through the Islands of Bali in a Private Cruise"
           />
@@ -24,19 +24,19 @@ function Cards() {
         <div className="cards-items-container">
           <CardItem
             path="/products"
-            img="/images/img-3.jpg"
+            img="images/img-3.jpg"
             label="Mystery"
             text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
           />
           <CardItem
             path="/products"
-            img="/images/img-4.jpg"
+            img="images/img-4.jpg"
             label="Amazing"
             text="Experience Football on Top of the Himilayan Mountains"
           />
           <CardItem
             path="/products"
-            img="/images/img-8.jpg"
+            img="images/img-8.jpg"
             label="Adrenaline"
             text="Ride through the Sahara Desert on a guided camel tour"
           />
